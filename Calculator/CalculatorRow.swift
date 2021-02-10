5f96a2787ffe77cf2d1cdb03a76496b384e0b2a6
Yasuko Kurata <yasukokurata@gmail.com>
2021-02-10 12:25:24 -0500

    create calculator row

--- Calculator/CalculatorRow.swift
+++ Calculator/CalculatorRow.swift
@@ -7,14 +7,27 @@
 
 import SwiftUI
 
+let columnCount = 4
+
 struct CalculatorRow: View {
+    var labels = ["", "", "", ""]
+    var colors:[Color] = [.gray, .gray, .gray, .orange]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Display a caluculator button for each column
+        HStack(spacing: 10) {
+            // Select from the labels and colors
+            ForEach(0..<columnCount) { index in
+                CalculatorButton(label: labels[index], color: colors[index])
+                
+            }
+        }
     }
 }
 
 struct CalculatorRow_Previews: PreviewProvider {
     static var previews: some View {
-        CalculatorRow()
+        CalculatorRow(labels: ["1", "2", "3", "+"])
+            .previewLayout(.fixed(width: 300, height: 100))
     }
 }
