949e10cea51997718c09ec2bf02eee774c594f85
Yasuko Kurata <yasukokurata@gmail.com>
2021-02-09 23:49:09 -0500

    initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Yasuko Kurata on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
